76a675306e368fde5120e90de2575630abf42164
haiming <brave.yhm@gmail.com>
2024-01-08 17:45:52 +0800

    建站历程

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/7c62e317.html","posts/adc9b3fe.html","posts/cd52224a.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/4a17b156.html","posts/cd52224a.html","posts/8f1686ee.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
