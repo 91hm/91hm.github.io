189f655b4be35e99e0beec896dd67e80ddcf7c75
haiming <brave.yhm@gmail.com>
2024-01-09 00:52:21 +0800

    细节调整

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/4a17b156.html","posts/cd52224a.html","posts/8f1686ee.html"];function toRandomPost(){
+var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/cd52224a.html","posts/8f1686ee.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
