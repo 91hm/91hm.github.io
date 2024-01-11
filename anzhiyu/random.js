bc43f2da795ef59cd99711f25fad7435ef4d3baa
haiming <brave.yhm@gmail.com>
2024-01-11 15:43:04 +0800

    更新音乐盒

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/8f1686ee.html","posts/cd52224a.html"];function toRandomPost(){
+var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/cd52224a.html","posts/8f1686ee.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
