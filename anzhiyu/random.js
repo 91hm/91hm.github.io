12b0a0e9b423bc98dd149878652ea4eb4f6fd216
widder <brave.yhm@gmail.com>
2024-01-13 12:31:25 +0800

    更新音乐盒

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/49a026b8.html","posts/6d3a0a06.html","posts/8f1686ee.html","posts/cd52224a.html"];function toRandomPost(){
+var posts=["posts/adc9b3fe.html","posts/7c62e317.html","posts/49a026b8.html","posts/6d3a0a06.html","posts/cd52224a.html","posts/8f1686ee.html","posts/1a1b695f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
