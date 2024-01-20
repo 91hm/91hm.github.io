a71f0bdd2d94b32d9032621df68930992ee5795d
widder <brave.yhm@gmail.com>
2024-01-20 23:35:32 +0800

    更新音乐盒

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/adc9b3fe.html","posts/0.html","posts/49a026b8.html","posts/7c62e317.html","posts/6d3a0a06.html","posts/cd52224a.html","posts/8f1686ee.html","posts/dac40ba7.html","posts/1a1b695f.html"];function toRandomPost(){
+var posts=["posts/542c2ed6.html","posts/adc9b3fe.html","posts/7c62e317.html","posts/6d3a0a06.html","posts/49a026b8.html","posts/dac40ba7.html","posts/cd52224a.html","posts/8f1686ee.html","posts/1a1b695f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
