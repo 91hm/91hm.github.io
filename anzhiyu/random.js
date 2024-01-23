666b6a850f91257246aa0967049f4dfd4f42085a
widder <brave.yhm@gmail.com>
2024-01-23 13:49:18 +0800

    更新音乐盒

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/adc9b3fe.html","posts/542c2ed6.html","posts/2d764f9d.html","posts/9f90b665.html","posts/1bbf064f.html","posts/55e45a70.html","posts/ff4f9ac7.html","posts/555df561.html","posts/a6e157d3.html","posts/ee9ce046.html","posts/7c62e317.html","posts/49a026b8.html","posts/45229b10.html","posts/6d3a0a06.html","posts/cd52224a.html","posts/b49dc1a8.html","posts/dac40ba7.html","posts/8f1686ee.html","posts/1a1b695f.html"];function toRandomPost(){
+var posts=["posts/adc9b3fe.html","posts/542c2ed6.html","posts/9f90b665.html","posts/2d764f9d.html","posts/555df561.html","posts/1bbf064f.html","posts/ff4f9ac7.html","posts/55e45a70.html","posts/a6e157d3.html","posts/7c62e317.html","posts/45229b10.html","posts/ee9ce046.html","posts/49a026b8.html","posts/6d3a0a06.html","posts/b49dc1a8.html","posts/dac40ba7.html","posts/8f1686ee.html","posts/cd52224a.html","posts/1a1b695f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
